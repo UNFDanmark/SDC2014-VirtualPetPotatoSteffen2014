a23f1c72c5bef39b511cb61a20e81449adc93550
sdc <github@software.unf.dk>
2014-07-15 16:20:51 +0200

    Nyt layout

--- src/dk/lott/VPPS2015/Potato.java
+++ src/dk/lott/VPPS2015/Potato.java
@@ -6,32 +6,14 @@ import java.util.Set;
  * Created by sdc on 7/15/14.
  */
 public class Potato {
-    int hunger=50;
-    int happiness=50;
-    int eat;
+    int hunger = 5;
+    int maxHunger = 5;
+    int happiness = 5;
+    int maxHappiness = 5;
+    int thirst = 5;
+    int energi = 5;
+    boolean death = false;
 
-    /**
-     * Hunger
-     */
-    if (hunger > 100) {
-        hunger = 100;
-    }
-    else if (hunger <= 50) {
 
-    }
-    else if (hunger <= 0) {
-        hunger = 0;
-        //Die somehow
-    }
-    if (eat) {
-        hunger+20;
-    }
-    /**
-     * Happiness
-     */
 
-    if (happiness <=0) {
-
-    }
-    //Evt. Flere Ting
 }
