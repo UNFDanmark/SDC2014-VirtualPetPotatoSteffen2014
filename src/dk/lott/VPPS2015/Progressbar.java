0ae4c25da0f1ade52211083675aa210a6446008a
sdc <github@software.unf.dk>
2014-07-15 19:32:12 +0200

    update on: Prograssbar

--- src/dk/lott/VPPS2015/Progressbar.java
+++ src/dk/lott/VPPS2015/Progressbar.java
@@ -4,11 +4,5 @@ package dk.lott.VPPS2015;
  * Created by sdc on 7/15/14.
  */
 public class Progressbar {
-Potato potato = new Potato();
-
-    boolean EnergiBar{
-        if (potato.maxEnergi)
-    } else if (potato.energi) {
-
-    } else (potato.minEnergi)
+    Potato potato = new Potato();
 }
