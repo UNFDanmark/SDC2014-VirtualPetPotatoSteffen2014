ff9e4fa433adcd8ffb585d3cfe6c8b270918c8c3
sdc <github@software.unf.dk>
2014-07-16 19:29:44 +0200

    Notifikationer lavet

--- src/dk/lott/VPPS2015/PotatoService.java
+++ src/dk/lott/VPPS2015/PotatoService.java
@@ -1,9 +1,6 @@
 package dk.lott.VPPS2015;
 
-import android.app.AlarmManager;
-import android.app.IntentService;
-import android.app.NotificationManager;
-import android.app.PendingIntent;
+import android.app.*;
 import android.content.Context;
 import android.content.Intent;
 import android.preference.PreferenceManager;
@@ -34,23 +31,32 @@ public class PotatoService extends IntentService {
 * notificationer
 */
 
-    NotificationManager notificationManager = (NotificationManager)
-        getSystemService(NOTIFICATION_SERVICE);
-
-    Intent intent = new Intent(this, NotificationReceiver.class);
-    PendingIntent pIntent = PendingIntent.getActivity(this, 0, intent, 0);
-
-
-
     @Override
     public int onStartCommand(Intent intent, int flags, int startId) {
         Potato potato = new Potato();
         potato.load(PreferenceManager.getDefaultSharedPreferences(getApplicationContext()));
-        if(potato.hunger == 200){
-
+        if(potato.hunger <= 200){
+        showNotification("I am hungry! Feed me, please!");
+        }
+        else if(potato.energy <= 200){
+            showNotification("So tired.....");
+        }
+        else if(potato.thirst<= 200){
+            showNotification("No water.The end is near");
+        }
+        else if(potato.happiness <= 200){
+            showNotification("So lonely... U y no love me anymore!?!");
         }
 
         return super.onStartCommand(intent, flags, startId);
+    }
+
+    public void showNotification(String besked){
+        Notification notification = new Notification.Builder(this).setContentText(besked).setSmallIcon(R.drawable.trist).
+                build();
+        NotificationManager manager = (NotificationManager) getSystemService(NOTIFICATION_SERVICE);
+        manager.notify(7, notification);
+
     }
 
 /**
@@ -60,6 +66,6 @@ public class PotatoService extends IntentService {
         Intent intent = new Intent(context, PotatoService.class);
         PendingIntent pintent = PendingIntent.getService(context, 0, intent, 0);
         AlarmManager alarm = (AlarmManager)context.getSystemService(Context.ALARM_SERVICE);
-        alarm.setRepeating(AlarmManager.RTC_WAKEUP, System.currentTimeMillis(), 1000*60*30, pintent);
+        alarm.setRepeating(AlarmManager.RTC_WAKEUP, System.currentTimeMillis(), 1000*60, pintent);
     }
 }
